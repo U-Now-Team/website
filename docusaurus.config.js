cef33fcc09b274744c6abc97bfa9e75bbcf5a7a5
Milir <13410850124@163.com>
2026-03-11 17:32:26 +0800

    feat: delete default tutorial and add ai development guide

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,31 +1,24 @@
-import { themes as prismThemes } from "prism-react-renderer";
-import type { Config } from "@docusaurus/types";
-import type * as Preset from "@docusaurus/preset-classic";
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prism_react_renderer_1 = require("prism-react-renderer");
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
-const config: Config = {
+var config = {
   title: "U Know, U Now",
   favicon: "img/favicon.ico",
-
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
-
   // Set the production url of your site here
   url: "https://u-now-team.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/website/",
-
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "U-Now-Team", // Usually your GitHub org/user name.
   projectName: "website", // Usually your repo name.
-
   onBrokenLinks: "throw",
-
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
@@ -33,7 +26,6 @@ const config: Config = {
     defaultLocale: "en",
     locales: ["en"],
   },
-
   presets: [
     [
       "classic",
@@ -47,10 +39,9 @@ const config: Config = {
         theme: {
           customCss: "./src/css/custom.css",
         },
-      } satisfies Preset.Options,
+      },
     ],
   ],
-
   themeConfig: {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
@@ -91,13 +82,15 @@ const config: Config = {
     },
     footer: {
       style: "dark",
-      copyright: `Copyright © ${new Date().getFullYear()} U-Now Team. All rights reserved.`,
+      copyright: "Copyright \u00A9 ".concat(
+        new Date().getFullYear(),
+        " U-Now Team. All rights reserved.",
+      ),
     },
     prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
+      theme: prism_react_renderer_1.themes.github,
+      darkTheme: prism_react_renderer_1.themes.dracula,
     },
-  } satisfies Preset.ThemeConfig,
+  },
 };
-
-export default config;
+exports.default = config;
